43e5e5c1545396f2d13b458499b2e06253e2f3d0
Maurice <maurice@monadius.com>
2024-06-01 18:44:49 +0200

    Dealer cards class mapping

---
+++ api/dealer_class_mapping2.py
@@ -0,0 +1,16 @@
+# Dictionary mapping class IDs to card types
+dealer_class_mapping = {
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "8",
+    "9": "9",
+    "10": "10",
+    "J": "Jack",
+    "Q": "Queen",
+    "K": "King",
+    "A": "Ace",
+}
