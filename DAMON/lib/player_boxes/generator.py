c994c0cbb4c1ebf0b5de03e956c756c84236c910
Maurice <mauriceboendermaker@gmail.com>
2024-06-06 22:18:55 +0200

    Removed and reused code

--- DAMON/lib/player_boxes/generator.py
+++ DAMON/lib/player_boxes/generator.py
@@ -1,79 +1,49 @@
 import cv2
 import numpy as np
-from PIL import Image, ImageTk
-from roboflow import Roboflow
-from mss import mss
-from matplotlib.path import Path
 import tkinter as tk
+
+from PIL import Image, ImageTk
 from tkinter import messagebox
 
 from ..common import constants
+from ..logic.monitor_utils import MonitorUtils
+from ..logic.utils import Utils
 
 
 class PlayerBoxGenerator:
     def __init__(self, gui):
         self.gui = gui
-        self.monitor = None
-        self.roboflow_model = self._initialize_roboflow_model()
+        self.model_players = Utils.initialize_player_model(self)
         self.current_image_path = None
-
-    def _initialize_roboflow_model(self):
-        rf = Roboflow(api_key=constants.ROBOFLOW_API_KEY)
-        project = rf.workspace().project(constants.ROBOFLOW_PROJECT_ID)
-        return project.version(constants.ROBOFLOW_MODEL_VERSION).model
+        self.monitor_utils = MonitorUtils()
 
     def set_monitor(self, monitor):
-        self.monitor = monitor
+        self.monitor_utils.set_monitor(monitor)
 
     def generate(self):
-        if not self.monitor:
+        if not self.monitor_utils.monitor:
             messagebox.showerror("Error", "Monitor not selected.")
             return
 
         current_resolution = self._capture_screen_and_predict()
-        scale_x, scale_y = self._get_scaling_factors(constants.BASE_RESOLUTION, current_resolution)
-        player_regions = self._scale_player_regions(constants.BASE_PLAYER_REGIONS, scale_x, scale_y)
+        scale_x, scale_y = self.monitor_utils.get_scaling_factors(constants.BASE_RESOLUTION, current_resolution)
+        player_regions = self.monitor_utils.scale_player_regions(constants.BASE_PLAYER_REGIONS, scale_x, scale_y)
         self._draw_player_regions_on_image(constants.OUTPUT_PREDICTION_PATH, player_regions)
         self.current_image_path = constants.OUTPUT_FINAL_PREDICTION_PATH
         self._display_image(self.current_image_path)
 
     def _capture_screen_and_predict(self):
-        monitor_info = self.monitor
-        monitor = {
-            "left": monitor_info.x,
-            "top": monitor_info.y,
-            "width": monitor_info.width,
-            "height": monitor_info.height
-        }
-        sct = mss()
-        sct_img = sct.grab(monitor)
-        img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
+        img = self.monitor_utils.capture_screen()
+
         img.save(constants.INPUT_SCREENSHOT_PATH)  # Save screenshot for prediction
 
         # Use model to predict on the saved screenshot and save the prediction result
-        self.roboflow_model.predict(
-            constants.INPUT_SCREENSHOT_PATH, confidence=constants.PREDICTION_CONFIDENCE,
-            overlap=constants.PREDICTION_OVERLAP
+        self.model_players.predict(
+            constants.INPUT_SCREENSHOT_PATH, confidence=constants.PREDICTION_CONFIDENCE_PLAYERS,
+            overlap=constants.PREDICTION_OVERLAP_PLAYERS
         ).save(constants.OUTPUT_PREDICTION_PATH)
 
-        return (monitor_info.width, monitor_info.height)
-
-    def _get_scaling_factors(self, base_resolution, current_resolution):
-        base_width, base_height = base_resolution
-        current_width, current_height = current_resolution
-        scale_x = current_width / base_width
-        scale_y = current_height / base_height
-        return scale_x, scale_y
-
-    def _scale_player_regions(self, base_player_regions, scale_x, scale_y):
-        scaled_player_regions = []
-        for region in base_player_regions:
-            scaled_vertices = []
-            for vertex in region:
-                scaled_vertex = [vertex[0] * scale_x, vertex[1] * scale_y]
-                scaled_vertices.append(scaled_vertex)
-            scaled_player_regions.append(Path(np.array(scaled_vertices)))
-        return scaled_player_regions
+        return self.monitor_utils.capture_screen().width, self.monitor_utils.capture_screen().height
 
     def _draw_player_regions_on_image(self, image_path, player_regions):
         img = cv2.imread(image_path)
